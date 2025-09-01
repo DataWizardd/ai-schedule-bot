27017a751fa46ee0178d4c8b4ca408f74fa2042a
JaeHeung Park <117520527+DataWizardd@users.noreply.github.com>
2025-09-01 22:45:16 +0900

    clean up changes

--- smoke_ptb.py
+++
@@ -1,4 +0,0 @@
-from telegram.ext import ApplicationBuilder
-print("building...")
-app = ApplicationBuilder().token("123456789:INVALID_TOKEN_FOR_SMOKETEST").build()
-print("OK: Application built (token invalid is fine)")
